1d0dea83d57aa369578f2e0ed1e969ba86696501
evandikt <evandikt@gmail.com>
2021-09-10 13:18:09 +0200

    TCP

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import process from "process";
-import { getEvntComServerFromChildProcess } from "evntboard-communicate";
+require("dotenv").config();
+import {EvntComNode} from "evntcom-js/dist/node";
 import * as googleTTS from 'google-tts-api';
 
-const { config: { lang: LANG } } = JSON.parse(process.argv[2]);
+const NAME: string = process.env.EVNTBOARD_NAME || "google-tts";
+const HOST: string = process.env.EVNTBOARD_HOST || "localhost";
+const PORT: number = process.env.EVNTBOARD_PORT ? parseInt(process.env.EVNTBOARD_PORT) : 5001;
+const LANG: string = process.env.EVNTBOARD_CONFIG_LANG || 'en';
 
-const evntComServer = getEvntComServerFromChildProcess();
+const evntCom = new EvntComNode({
+    name: NAME,
+    port: PORT,
+    host: HOST,
+});
 
-let tts_lang = LANG || 'en'
+let tts_lang = LANG;
 
 const setLang = async (lang:string) => {
     tts_lang = lang;
@@ -23,6 +30,6 @@ const generate = async (message:string, lang?: string, splitPunct?: string) => {
     }
 }
 
-evntComServer.expose("setLang", setLang);
-evntComServer.expose("getLang", getLang);
-evntComServer.expose("generate", generate);
+evntCom.expose("setLang", setLang);
+evntCom.expose("getLang", getLang);
+evntCom.expose("generate", generate);
